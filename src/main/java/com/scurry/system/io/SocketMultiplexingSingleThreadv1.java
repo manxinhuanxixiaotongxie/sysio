d37ccc08d2e57b8f86d8c815530e670d8f704380
yanghm55 <yanghm55@mideqa.com>
2025-10-28 10:07:53 +0800

    IO注释

--- src/main/java/com/scurry/system/io/SocketMultiplexingSingleThreadv1.java
+++ src/main/java/com/scurry/system/io/SocketMultiplexingSingleThreadv1.java
@@ -1,4 +1,4 @@
-package com.scurry.system.io;
+package src.main.java.com.scurry.system.io;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -7,18 +7,46 @@ import java.nio.channels.*;
 import java.util.Iterator;
 import java.util.Set;
 
+/**
+ * 多路复用器
+ *
+ */
 public class SocketMultiplexingSingleThreadv1 {
 
+    /**
+     * 参考IO部分内核演进部分
+     *
+     */
     private ServerSocketChannel server = null;
+    // selector 基于 select poll epoll kqueue进行封装
+    // 多路复用器
     private Selector selector = null;
     int port = 9090;
 
     public void initServer() {
         try {
+            /**
+             * BIO java提供非阻塞式的Socket编程接口
+             */
             server = ServerSocketChannel.open();
             server.configureBlocking(false);
+            // 会创建监听的文件描述符
             server.bind(new InetSocketAddress(port));
+            /** 如果是epoll会创建epoll的文件描述符
+             * 相当于调用epoll_create
+            如果是在poll模型下 会在jvm内部创建对象保存
+            注意 内核的演进 select poll 支持多个文件描述符的监听 但是对于java代码来说 不需要关心多个文件描述符的维护
+
+             jvm默认使用epoll模型 但是可以使用-D进行指定使用的多路复用器
+
+
+            **/
             selector = Selector.open();  //  select  poll  *epoll
+            /**
+             * 注册的是事件类型
+             * 对poll模型来说
+             *
+             */
             server.register(selector, SelectionKey.OP_ACCEPT);
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/com/scurry/system/io/SocketNIO.java
+++ src/main/java/com/scurry/system/io/SocketNIO.java
@@ -1,4 +1,4 @@
-package com.scurry.system.io;
+package src.main.java.com.scurry.system.io;
 
 import java.net.InetSocketAddress;
 import java.net.StandardSocketOptions;
@@ -7,6 +7,10 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.LinkedList;
 
+/**
+ * NIO 非阻塞式IO
+ *
+ */
 public class SocketNIO {
 
     public static void main(String[] args) throws Exception {
@@ -14,7 +18,9 @@ public class SocketNIO {
         LinkedList<SocketChannel> clients = new LinkedList<>();
 
         ServerSocketChannel ss = ServerSocketChannel.open();
+        // 绑定端口
         ss.bind(new InetSocketAddress(9090));
+        // 设置非阻塞
         ss.configureBlocking(false); //重点  OS  NONBLOCKING!!!
 
         ss.setOption(StandardSocketOptions.TCP_NODELAY, false);
@@ -26,26 +32,59 @@ public class SocketNIO {
 //        StandardSocketOptions.SO_REUSEADDR
 
 
-
+        /**
+         * 非阻塞时代
+         * 注意：这里并没有使用多路复用器
+         * 多路复用器是在select poll epoll  kqueue出现之后才有的
+         *
+         * 不阻塞
+         */
 
         while (true) {
             Thread.sleep(1000);
+            // 设置非阻塞  非阻塞
             SocketChannel client = ss.accept(); //不会阻塞？  -1NULL
 
             if (client == null) {
                 System.out.println("null.....");
             } else {
+                // 新连接进来
                 client.configureBlocking(false);
                 int port = client.socket().getPort();
                 System.out.println("client...port: " + port);
+                // 连接创建
+                /**
+                 * 但是对于服务端来说
+                 * 尽管已经创建了连接 但是那些连接是就绪的或者说那些连接有数据包发送过来是不清楚的
+                 * 因此必须要进行遍历 查看具体是哪个连接有数据到达
+                 *
+                 * 要理解这个地方 必须要参考IO模块的内容
+                 * 程序都是是工作在用户态的
+                 * 因此这个地方发生的轮训都是发生在用户态
+                 */
                 clients.add(client);
             }
 
             ByteBuffer buffer = ByteBuffer.allocateDirect(4096);  //可以在堆里   堆外
 
+            /**
+             * 数据到达之后其实计算机等相关的很多个硬件软件都参与了复杂的过程
+             * 包括资源的开辟  数据的复制 内存的分配以及一系列中断等等
+             *
+             * 数据包网络传输  参考网络部分的资料
+             * 计算机的数据处理以及中断以及数据拷贝等方面的内容参考计算机组成以及IO部分
+             *
+             * 数据包达到之后 说明可写
+             */
             for (SocketChannel c : clients) {   //串行化！！！！  多线程！！
+                // 进行频繁的用户态内核态切换
                 int num = c.read(buffer);  // >0  -1  0   //不会阻塞
                 if (num > 0) {
+                    // 读取之间将pos指针以及limit指针放置在正确的位置开始读取
+                    // 注意这里读取的是文件描述符所描述的文件内容
+                    // 针对连接的内容
+                    // 使用lsof -op pid 能看到具体的描述信息
+                    // 包括数据包接受的、数据包发送的等信息
                     buffer.flip();
                     byte[] aaa = new byte[buffer.limit()];
                     buffer.get(aaa);
