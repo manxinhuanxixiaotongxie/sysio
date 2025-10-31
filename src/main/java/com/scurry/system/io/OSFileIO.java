f0fc618d1b58de9f0086caea12643f7f7620e0dd
yanghm55 <yanghm55@mideqa.com>
2025-10-31 18:20:26 +0800

    IO详细流程

--- src/main/java/com/scurry/system/io/OSFileIO.java
+++ src/main/java/com/scurry/system/io/OSFileIO.java
@@ -85,6 +85,44 @@ public class OSFileIO {
         FileChannel rafchannel = raf.getChannel();
         // lsof -op 多一个的mem fd  要么使用buffer要么使用fd进行文件的读写
         //mmap  堆外  和文件映射的   byte  not  object
+
+        /**
+         * 标准nio 并没有实现mmap（）方法
+         *
+         * 原因：
+         *   1.数据载体是 可随机访问 持久化的缓冲区（磁盘空间的页缓存） 这类缓冲区生命周期稳定 地址空间可被安全映射
+         *   2.数据操作双向可同步  用户态修改映射区域侯 内核可通过页表同步到底层存储（如磁盘）反之亦然
+         *
+         *   网络IO不适合直接实现mmap()
+         *      网络IO是socket缓冲区（内核维护的接受 发送缓冲区） 其特性与磁盘文件的页缓存有本质区别  导致mmap难以直接使用
+         *
+         *      1.网络缓冲区的临时性与不可控性
+         *          生命周期短暂
+         *          大小动态变化
+         *          数据不可随机访问
+         *      2.安全性与并发风险
+         *         内核与用户态同步问题
+         *         权限控制困难
+         *      3.跨平台性与内核接口差异
+         *      4.替代方案：
+         *      java NIO提供了更适合的高效方案
+         *        1.直接缓冲区  DirectByteBuffer    ByteBuffer.allocate(8192);堆外  linux堆内    ByteBuffer.allocateDirect(1024);linux堆外
+         *
+         *        2.多路复用
+         *
+         *
+         *
+         *
+         *
+         *
+         *   文件IO
+         *      1.文件数据持久化
+         *      2.支持随机访问
+         *      3.同步机制成熟  受到内核控制
+         *      4.跨平台支持好
+         *
+         *
+         */
         MappedByteBuffer map = rafchannel.map(FileChannel.MapMode.READ_WRITE, 0, 4096);
 
         // 不是系统调用  但是数据会到达 内核的pagecache
